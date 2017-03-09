e61c200598303d20af0c37a98b8caeed957e1241
Diego Medina <fmpwizard@gmail.com>
2017-03-09 16:26:22 -0500

    cleaned code before posting for help

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"fmt"
 	"go/ast"
-	"go/doc"
 	"go/parser"
 	"go/token"
 	"go/types"
-	"io"
-	"os"
-	"strings"
 )
 
 var src = `
@@ -53,101 +47,23 @@ func findByName(name string) {
 }
 `
 
-// AppInfo holds all the models we collected
-type AppInfo struct {
-	Models []Model
-}
-
-// Model has the collection name and a slice of Fields. It also has the package where the struct was defined
-type Model struct {
-	Package string
-	Name    string
-	Fields  []Field
-}
-
-// Field holds the field's name and type
-type Field struct {
-	Name string
-	Type string
-}
-
-var appInfo AppInfo
-
-var kPath = "./seeddata"
 var report = false
 var fset *token.FileSet
 
 func main() {
 	fset = token.NewFileSet()
 
-	/*
-		pkgs, e := parser.ParseDir(fset, kPath, nil, parser.ParseComments)
-		if e != nil {
-			log.Fatal(e)
-			return
-		}
-
-		astf := make([]*ast.File, 0)
-		for _, pkg := range pkgs {
-
-			fmt.Printf("package %v\n", pkg.Name)
-			for fn, f := range pkg.Files {
-				fmt.Printf("file %v\n", fn)
-				astf = append(astf, f)
-			}
-		}
-	*/
-
-	/*
-		config := &types.Config{
-			Error: func(e error) {
-				fmt.Println(e)
-			},
-			Importer: importer.Default(),
-		}
-	*/
 	info := types.Info{
 		Types: make(map[ast.Expr]types.TypeAndValue),
 		Defs:  make(map[*ast.Ident]types.Object),
 		Uses:  make(map[*ast.Ident]types.Object),
 	}
-	/*
-		pkg, e := config.Check(kPath, fset, astf, &info)
-		if e != nil {
-			fmt.Println(e)
-		}
-		fmt.Printf("types.Config.Check got %v\n", pkg.String())
-	*/
-	//for _, pkg := range pkgs {
-	//getDocs(pkg)
-	//}
 
 	f, err := parser.ParseFile(fset, "sample.go", src, parser.ParseComments)
 	if err != nil {
 		fmt.Println("failed to parse file ", err)
 	}
 	ast.Walk(&PrintASTVisitor{&info}, f)
-
-	/*
-		for _, f := range astf {
-			ast.Walk(&PrintASTVisitor{&info}, f)
-		}
-	*/
-
-}
-
-func getDocs(pkg *ast.Package) {
-	p := doc.New(pkg, "./", 0)
-	for _, t := range p.Types {
-		//fmt.Println("  type", t.Name)
-		//fmt.Printf("    docs:\n%v", t.Doc)
-		if isMgoStruct(t.Doc) {
-			m := getMgoCollName(t.Doc)
-			fmt.Printf("collection name: %s\n", m)
-		}
-	}
-	fmt.Println("step ======================= step")
-
 }
 
 type PrintASTVisitor struct {
@@ -193,126 +109,4 @@ func (v *PrintASTVisitor) Visit(node ast.Node) ast.Visitor {
 		}
 	}
 	return v
-}
-
-func main1() {
-	//filepath.Walk("./seeddata", walk)
-	//fmt.Println("=============")
-	docs()
-}
-
-func docs() {
-	fset := token.NewFileSet() // positions are relative to fset
-
-	d, err := parser.ParseDir(fset, "./seeddata", nil, parser.ParseComments)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	for _, f := range d {
-		//fmt.Println("package", k)
-		p := doc.New(f, "./", 0)
-		for _, t := range p.Types {
-			//fmt.Println("  type", t.Name)
-			//fmt.Printf("    docs:\n%v", t.Doc)
-			if isMgoStruct(t.Doc) {
-				m := getMgoCollName(t.Doc)
-				fmt.Printf("collection name: %s\n", m)
-			}
-		}
-		fmt.Println("step ======================= step")
-		ast.Inspect(f, func(n ast.Node) bool {
-			var s string
-			switch x := n.(type) {
-			case *ast.FuncLit:
-				fmt.Println("xxxxxxxxxxxxxxxxxxxxxx")
-				for _, v := range x.Body.List {
-					fmt.Printf("fn: %+v\n", v)
-				}
-			case *ast.FuncDecl:
-				fmt.Println("---------------------------")
-				fmt.Printf("fn: %+v\n", x.Name)
-
-			/*case *ast.BasicLit:
-				//fmt.Println("a")
-				s = x.Value
-			case *ast.Ident:
-				//fmt.Println("b")
-				if x.Obj != nil {
-					fmt.Println("c: ", x.Obj.Kind.String())
-					switch d := x.Obj.Decl.(type) {
-					case *ast.Field:
-						fmt.Printf("field Tag: %s\n", d.Tag.Value)
-						fmt.Printf("field Type: %+v\n", d.Type)
-						for _, n := range d.Names {
-							fmt.Printf("field Name: %+v\n", n)
-						}
-					}
-				}
-				s = x.Name
-			*/
-			default:
-				fmt.Printf("00000000000000000000000 %T\n", x)
-
-			}
-			if s != "" {
-				fmt.Printf("%s:\t%s\n", fset.Position(n.Pos()), s)
-			}
-			return true
-		})
-
-	}
-}
-
-func walk(path string, info os.FileInfo, err error) error {
-	fmt.Println("reading: ", path)
-	if strings.HasSuffix(info.Name(), ".go") {
-		findDirective(path)
-	}
-	return err
-}
-
-func findDirective(fpath string) error {
-	f, err := os.Open(fpath)
-	if err != nil {
-		return err
-	}
-	input := bufio.NewReader(f)
-	for {
-		var buf []byte
-		buf, err = input.ReadSlice('\n')
-		if err == bufio.ErrBufferFull {
-			return bufio.ErrBufferFull
-		}
-		if err != nil {
-			// Check for marker at EOF without final \n.
-			if err == io.EOF && isMGoDirective(buf) {
-				err = io.ErrUnexpectedEOF
-				return err
-			}
-			break
-		}
-		if isMGoDirective(buf) {
-			fmt.Println(string(buf))
-		}
-	}
-
-	return nil
-}
-
-func isMgoStruct(in string) bool {
-	return strings.Contains(in, "mgo:model")
-}
-
-func getMgoCollName(in string) string {
-	x := strings.Split(in, ":")
-	if len(x) == 3 {
-		return strings.TrimSpace(x[2])
-	}
-	return ""
-}
-
-func isMGoDirective(in []byte) bool {
-	return bytes.HasPrefix(in, []byte("//mgo:model"))
 }
