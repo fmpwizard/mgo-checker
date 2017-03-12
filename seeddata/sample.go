15a18a26ff1e9f707c427dcd1b10cc69d071ee7f
Diego Medina <fmpwizard@gmail.com>
2017-03-11 22:14:29 -0500

    simpler sample.go code

--- seeddata/sample.go
+++ seeddata/sample.go
@@ -3,7 +3,6 @@ package seeddata
 import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"time"
 )
 
 // Company represents a Company document in mongodb
@@ -15,23 +14,13 @@ type Company struct {
 	Zip string `bson:"zip_code"`
 }
 
-// User represents a user document in mongodb
-// mgo:model:xyz_users
-type User struct {
-	// Name the user name
-	Name string `bson:"name"`
-	// Email the uer's email
-	Email string `bson:"email"`
-}
-
 // connect is here
 func connect() *mgo.Session {
-	xyzWebSession, _ := mgo.DialWithTimeout("127.0.0.1:2700/dbname", 2*time.Second)
+	xyzWebSession, _ := mgo.DialWithTimeout("127.0.0.1:2700/dbname", 0)
 	return xyzWebSession
 }
 func findByName(name string) {
 	var ret []Company
-	collName := "xyz_company"
-	testCollection := connect().DB("dbname").C(collName)
+	testCollection := connect().DB("dbname").C("xyz_company")
 	testCollection.Find(bson.M{"name": 1}).All(&ret)
 }
